8cc96cb2119196b19b43094d511de14d86809146
Mahmoud-Elhoseny <elhoseny916@gmail.com>
2023-02-24 00:50:33 +0300

    .

--- main.js
+++ main.js
@@ -1,33 +1,24 @@
 // let icons = document.querySelectorAll(".tasks i")
 // let infoTasks = document.querySelectorAll(".tasks .info")
-// console.log(icons);
-// console.log(infoTasks);
-
-
-
-// icons.forEach((i) => {
-//     i.addEventListener("click", deleteIcons)
-// })
 
 
 
 
+// function clicked() {
+//     infoTasks.forEach(task => {
+
+//         icons.forEach(icon => {
+//             icon.addEventListener("click", function (e) {
+//                 icon.remove(task)
+//             })
+//         })
+//     }
 
 
 
-// function deleteIcons() {
-//     icons.forEach(() => {
-//         this.classList.add("done")
-//     })
-//     infoTasks.forEach((e) => {
-//         e.classList.add("done")
-//     });
+// )}
 
-// }
-
-
-
-
+// clicked()
 
 
 
